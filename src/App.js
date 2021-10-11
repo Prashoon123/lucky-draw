f7c05a1761dca98625ced600bc7f9b8cc257226a
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2021-10-11 10:15:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+import "./App.css";
+import { db } from "./firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Spinner from "react-spinkit";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [participants, loading] = useCollection(db.collection("participants"));
+  const [winner, setWinner] = useState(null);
+  const winnerRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    db.collection("participants").add({
+      name: input,
+    });
+
+    setInput("");
+  };
+
+  const randomWinner = () => {
+    const randomParticipant = Math.floor(
+      Math.random() * participants?.docs.length
+    );
+    setWinner(participants?.docs[randomParticipant].data().name);
+    setTimeout(() => {
+      winnerRef.current.scrollIntoView({ behaviour: "smooth" });
+    }, 100);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // BEM naming convention
+    <div className="app">
+      <div style={{ height: 10 }} />
+      <form>
+        <TextField
+          label="Enter your name"
+          required
+          value={input}
+          variant="filled"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <div style={{ height: 10 }} />
+        <Button
+          variant="contained"
+          color="secondary"
+          type="submit"
+          onClick={handleSubmit}
+          disabled={!input}
         >
-          Learn React
-        </a>
-      </header>
+          Enter the lucky draw
+        </Button>
+      </form>
+      <div style={{ height: 10 }} />
+      <h2>Participants List -</h2>
+      {loading ? (
+        <>
+          <div style={{ height: 50 }} />
+          <Spinner name="ball-spin-fade-loader" />
+        </>
+      ) : (
+        ""
+      )}
+      <div style={{ height: 5 }} />
+      <h4>
+        <ol>
+          {participants?.docs.map((participant, index) => (
+            <>
+              <li key={index}>{participant.data().name}</li>
+            </>
+          ))}
+        </ol>
+      </h4>
+      <div style={{ height: 10 }} />
+      {participants && (
+        <Button variant="contained" color="primary" onClick={randomWinner}>
+          Give me a random winner
+        </Button>
+      )}
+      <div style={{ height: 5 }} />
+      {winner && (
+        <>
+          <h1 id="winner" ref={winnerRef}>
+            The winner is: {winner}
+          </h1>
+        </>
+      )}
     </div>
   );
 }
